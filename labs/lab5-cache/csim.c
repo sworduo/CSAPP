81c24fbd5997e2eb430074cbb240fa7dac81fe66
sworduo <sworduo@163.com>
2019-06-26 12:54:22 +0800

    lab5-Done

--- labs/lab5-cache/csim.c
+++ labs/lab5-cache/csim.c
@@ -1,10 +1,258 @@
+//===================================================================================================
+//author:L
+//date:6,24,2019,Monday,Rain
+//=======       =======       =======       =======
+//       =     =       =     =       =     =
+//        =   =         =   =         =   =
+//          =             =             =
+//        =   =         =   =         =   =
+//       =     =       =     =       =     =
+//=======       =======       =======       =======
+//===================================================================================================
+
 #include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define bool int
+#define true 1
+#define false 0
 
+int hit_count = 0;
+int miss_count = 0;
+int eviction_count = 0;
 
+//===================================================================================================
+//读取参数，初始化cache
+//===================================================================================================
+struct myCachePara{
+	int S;
+	int E;
+	int B;
+	int s;
+	int b;
+	bool verbose;
+	char *filename;
+};
 
-int main()
+void usage(char *func){
+	printf("Usage: %s [-hv] -s <s> -e <e> -b <b> -t <tracefile>\n", func);
+	exit(0);
+}
+
+int checkPara(char arg[], char *func){
+	//检查是否能读取相应参数
+	int len = strlen(arg);
+	int temp = 0;
+	for(int i=0; i<len; i++){
+		if(arg[i] < '0' || arg[i] > '9'){
+			usage(func);
+		}
+		//  数据量太小，不需要考虑溢出
+		temp = temp*10 + (arg[i] - '0');
+	}
+	return temp;
+}
+
+struct myCachePara parse(int argc, char* argv[]){
+	//解析输入参数，初始化缓存
+	if(argc < 2){
+		usage(argv[0]);
+	}
+	struct myCachePara para = {0, 0, 0, 0, 0, false};
+	bool s=false, e=false, b=false, t=false;
+	for(int i=1; i<argc; ++i){
+		if(strlen(argv[i]) != 2 || argv[i][0] != '-'){
+			usage(argv[0]);
+		}
+		char c = argv[i][1];
+		switch (c){
+			case 'h':
+				usage(argv[0]);
+			case 'v':
+				para.verbose = true;
+				continue;
+		}
+		//segt后面应该接一个参数
+		if(++i == argc){
+			usage(argv[0]);
+		}
+
+		switch(c){
+			case 's':
+			case 'S':
+				s = true;
+				para.s = checkPara(argv[i], argv[0]);
+				para.S = 1<<para.s;
+				break;
+			case 'e':
+			case 'E':
+				e = true;
+				para.E = checkPara(argv[i], argv[0]);
+				break;
+			case 'b':
+			case 'B':
+				b = true;
+				para.b = checkPara(argv[i], argv[0]);
+				para.B = 1<<para.b;
+				break;
+			case 't':
+			case 'T':
+				t = true;
+				para.filename = argv[i];
+				break;
+			default:
+				usage(argv[0]);
+		}	
+	}
+	if((s & e & b & t) == 0){
+		usage(argv[0]);
+	}
+	return para;
+}
+
+unsigned int toTen(char *addres){
+	//将字符串形式的6进制地址转换为10进制数字
+	unsigned int res = 0;
+	char c;
+	for(char *p = addres; *p != '\0'; p++){
+		c = *p;
+		if(c >= '0' && c <= '9'){
+			res = res * 16 + (c - '0');
+		} else if(c >= 'a' && c <= 'z'){
+			res = res * 16 + (c - 'a') + 10;
+		} else if(c >= 'A' && c <= 'Z'){
+			res = res * 16 + (c - 'A') + 10;
+		}else{
+			printf("Something wrong in decode addres\n");
+			printf("Wrong address:%c\n", c);
+			exit(3);
+		}
+	}
+	return res;
+}
+//===================================================================================================
+//判断是否命中
+//===================================================================================================
+void exectOp(int targets[], int used[], int E, int target, char *str){
+	int maxUsed = 0;
+	int longTimeNoUsed = -1; //最久没用过的项
+	int available = -1; //空闲项
+	bool hit = false;
+	for(int i=0; i<E; i++){
+		if(used[i] == -1){
+			available = i;
+		}
+		else if(target == targets[i]){
+			used[i] = 0;
+			hit = true;
+			//命中之后还要继续循环
+			//修改后面的时间戳
+		}
+		else if(++used[i] > maxUsed){
+			longTimeNoUsed = i;
+			maxUsed = used[i];
+		}
+	}
+
+	if(hit){
+		//hit
+		sprintf(str, "%s hit{target=%d}", str, target);
+		hit_count++;
+	}else{
+		sprintf(str, "%s miss{target=%d}", str, target);
+		miss_count++;
+		if(available == -1){
+			//没有空闲项
+			sprintf(str, "%s eviction{old target=%d}", str, targets[longTimeNoUsed]);
+			eviction_count++;
+			used[longTimeNoUsed] = 0;
+			targets[longTimeNoUsed] = target;
+		}else{
+			used[available] = 0;
+			targets[available] = target;
+		}
+	}
+}
+
+int main(int argc, char* argv[])
 {
-    printSummary(0, 0, 0);
+	// for(int i=0; i<argc; i++){
+	// 	printf("Argument %d is %s\n", i, argv[i]);
+	// }
+
+	struct myCachePara para = parse(argc, argv);
+	printf("Begin Cache! S=%d  E=%d  B=%d testfile=%s\n", para.S, para.E, para.B, para.filename);
+
+	int targets[para.S][para.E];
+	int used[para.S][para.E];//使用时间戳实现LRU
+	for(int i=0; i<para.S; ++i){
+		for(int j=0; j<para.E; ++j){
+			targets[i][j] = -1;
+			used[i][j] = -1; //-1表示这项无效
+		}
+	}
+
+	FILE *fp = fopen(para.filename, "r");
+	if(fp == NULL){
+		printf("Open file:%s failed!\n", para.filename);
+		exit(2);
+	}
+	char buf[255];
+	char c;
+	while(fgets(buf, 255, fp) != NULL){
+		c = buf[0];
+		if(c != 'I'){
+			//空格，代表接下来是M or L or S
+			//忽略I 
+			int sindex, target;
+			unsigned int addr;
+			char addres[100], str[100], op, size[100];
+			// int i=3;
+			// for(; i<255; i++){
+			// 	if(buf[i] != ','){
+			// 		addres[i-3] = buf[i];
+			// 	}else{
+			// 		addres[i-3] = '\0';
+			// 		i++;
+			// 		break;
+			// 	}
+			// }
+			// int j = i;
+			// for(; i<255; i++){
+			// 	if(buf[i] != '\n'){
+			// 		size[i-j] = buf[i];
+			// 	}else{
+			// 		size[i-j] = '\0';
+			// 		break;
+			// 	}
+			// }
+			sscanf(buf, " %c %[0-9a-zA-Z],%[0-9]", &op, addres, size);
+			addr = toTen(addres);
+			sindex = addr>>para.b;
+			sindex = sindex & ((1<<para.s) - 1);
+			target = addr>>(para.b+para.s);
+			sprintf(str, " %c %s,%s", op, addres, size);
+			exectOp(targets[sindex], used[sindex], para.E, target, str);
+			switch (op){
+			case 'L':
+			case 'S':			
+				break;
+			case 'M':
+				exectOp(targets[sindex], used[sindex], para.E, target, str);
+				break;
+			default:
+				printf("Unknown:%c   Something wrong in test file!\n", op);
+				exit(1);
+			}
+			if(para.verbose){
+				printf("%s s=%d\n", str, sindex);
+			}
+		}
+	}
+
+	fclose(fp);
+    printSummary(hit_count, miss_count, eviction_count);
     return 0;
 }
--- labs/lab5-cache/trans.c
+++ labs/lab5-cache/trans.c
@@ -8,9 +8,15 @@
  * on a 1KB direct mapped cache with a block size of 32 bytes.
  */ 
 #include <stdio.h>
+#include <stdlib.h>
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
+void transpose17(int M, int N, int A[N][M], int B[M][N]);
+void transpose32(int M, int N, int A[N][M], int B[M][N]);
+void transpose64(int M, int N, int A[N][M], int B[M][N]);
+void transpose64_3part(int M, int N, int A[N][M], int B[M][N]);
+void trans(int M, int N, int A[N][M], int B[M][N]);
 
 /* 
  * transpose_submit - This is the solution transpose function that you
@@ -22,8 +28,353 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    if(M == N){
+        if(M == 32){
+            transpose32(M, N, A, B);
+        }else if(M == 64){
+            transpose64_3part(M, N, A, B);
+        }
+        else{
+            trans(M, N, A, B);
+        }
+    }else if(M == 61 && N == 67){
+        transpose17(M, N, A, B);
+    }else{
+        trans(M, N, A, B);
+    }
 }
 
+int min(int a, int b){
+    return a>b?b:a;
+}
+
+void transpose17(int M, int N, int A[N][M], int B[M][N]){
+    int i, j, t, b;
+    for(i = 0; i < N; i += 16)
+        for(j = 0; j < M; j += 16)
+            for(t = i; t < min(i+16, N); t++)
+                for(b = j; b < min(j+16, M); b++){
+                        B[b][t] = A[t][b];
+                }
+}
+
+void transpose64(int M, int N, int A[N][M], int B[M][N]){
+    for(int i = 0; i < N; i += 4)
+        for(int j = 0; j < M; j += 4){
+            int a1=A[i][j], a2=A[i][j+1], a3=A[i][j+2], a4=A[i][j+3];
+            int a5=A[i+1][j], a6=A[i+1][j+1], a7=A[i+1][j+2], a8=A[i+1][j+3];
+            int a9=A[i+2][j], a10=A[i+2][j+1], a11=A[i+2][j+2], a12=A[i+2][j+3];
+            B[j][i] = a1;
+            B[j][i+1] = a5;
+            B[j][i+2] = a9;
+            B[j+1][i] = a2;
+            B[j+1][i+1] = a6;
+            B[j+1][i+2] = a10;
+            B[j+2][i] = a3;
+            B[j+2][i+1] = a7;
+            B[j+2][i+2] = a11;
+            B[j+3][i] = a4;
+            B[j+3][i+1] = a8;
+            B[j+3][i+2] = a12;
+            a1 = A[i+3][j];
+            a2 = A[i+3][j+1];
+            a3 = A[i+3][j+2];
+            a4 = A[i+3][j+3];
+            B[j][i+3] = a1;
+            B[j+1][i+3] = a2;
+            B[j+2][i+3] = a3;
+            B[j+3][i+3] = a4;
+        }
+}
+
+void transpose32(int M, int N, int A[N][M], int B[M][N]){
+    for(int i = 0; i < N; i += 8)
+        for(int j = 0; j < M; j += 8)
+            for(int b = 0; b < 8; b++){
+                int a1=A[i+b][j], a2=A[i+b][j+1], a3=A[i+b][j+2], a4=A[i+b][j+3];
+                int a5=A[i+b][j+4], a6=A[i+b][j+5], a7=A[i+b][j+6], a8=A[i+b][j+7];
+                B[j][i+b] = a1;
+                B[j+1][i+b] = a2;
+                B[j+2][i+b] = a3;
+                B[j+3][i+b] = a4;
+                B[j+4][i+b] = a5;
+                B[j+5][i+b] = a6;
+                B[j+6][i+b] = a7;
+                B[j+7][i+b] = a8;    
+            }   
+}
+
+char transpose64_3part_desc[] = "Transpose64 diagonal+orther+temp";
+void transpose64_3part(int M, int N, int A[N][M], int B[M][N]){
+    //左上角四个，对应缓存项0 8 16 24的缓存项作为中间值
+    int a1, a2, a3, a4, a5, a6, a7, a8, i, j, t, b;
+    for(i = 8; i < N; i += 8)
+        for(j = 8; j < N; j += 8)
+        if(i != j){
+            //处理8×8中的左上4×4小矩阵
+            for(t = 0; t < 4; t++)
+                for(b = 0; b < 4; b++){
+                    B[b+j][t+i] = A[t+i][b+j];
+                    //A上面4×8的小矩阵，左边4×4直接转置，右边放到B[0-4][0-4]保存
+                    B[t][b] = A[t+i][j+b+4];
+                }
+            for(t = 0; t < 4; t++)
+                for(b = 0; b < 4; b++){
+                    //一个缓存 八个数字，所以B[b+j[t+t+4]还在缓存中
+                    B[b+j][t+i+4] = A[t+i+4][b+j];
+                }
+            for(t = 0; t < 4; t++)
+                for(b = 0; b < 4; b++){
+                    B[b+j+4][t+i] = B[t][b];
+                    B[b+j+4][t+i+4] = A[t+i+4][b+j+4];
+                }
+        }
+        else{
+            //对角线A和B会冲突
+            //A上面两个4×4快
+            for(t = 0; t < 4; t++)
+                for(b = 0; b < 8; b++)
+                    B[t][b] = A[i+t][j+b];
+
+            for(t = 0; t < 4; t++)
+                for(b = 0; b < 4; b++)
+                    B[j+b][i+t] = B[t][b];
+
+            for(t = 0; t < 4; t++)
+                for(b = 4; b < 8; b++)
+                    B[j+b][i+t] = B[t][b];
+
+            //A下面两个4×4块
+             for(t = 4; t < 8; t++)
+                for(b = 0; b < 8; b++)
+                    B[t][b] = A[i+t][j+b];
+
+            for(t = 4; t < 8; t++)
+                for(b = 0; b < 4; b++)
+                    B[j+b][i+t] = B[t][b];
+
+            for(t = 4; t < 8; t++)
+                for(b = 4; b < 8; b++)
+                    B[j+b][i+t] = B[t][b];
+
+          }
+    
+    //处理第一列8个8×8块
+    //处理第一个对角块
+    i=0;
+    j=0;
+    for(t = 0; t < 4; t++){
+        a1=A[i+t][j], a2=A[i+t][j+1], a3=A[i+t][j+2], a4=A[i+t][j+3];
+        a5=A[i+t][j+4], a6=A[i+t][j+5], a7=A[i+t][j+6], a8=A[i+t][j+7];
+        B[j][i+t] = a1;
+        B[j+1][i+t] = a2;
+        B[j+2][i+t] = a3;
+        B[j+3][i+t] = a4;
+        //为防止冲突，剩下四个先放在右边4×4的子矩阵
+        B[j][i+t+4] = a5;
+        B[j+1][i+t+4] = a6;
+        B[j+2][i+t+4] = a7;
+        B[j+3][i+t+4] = a8;
+    }
+    //B左下矩阵，将右边的子矩阵放到下面
+    for(t = 0; t < 4; t++){
+        a1 = B[j+t][i+4];
+        a2 = B[j+t][i+5];
+        a3 = B[j+t][i+6];
+        a4 = B[j+t][i+7];
+        B[j+4+t][i] = a1;
+        B[j+4+t][i+1] = a2;
+        B[j+4+t][i+2] = a3;
+        B[j+4+t][i+3]= a4;
+    }
+    //处理B右上、右下两个子矩阵，完全冲突，只能4×4的块来搞。
+    //也就是处理A左下和右下两个子矩阵。
+    for(b = 0; b < 4; b++){
+            a1=A[i+4+b][j], a2=A[i+4+b][j+1], a3=A[i+4+b][j+2], a4=A[i+4+b][j+3];
+            B[j][i+4+b] = a1;
+            B[j+1][i+4+b] = a2;
+            B[j+2][i+4+b] = a3;
+            B[j+3][i+4+b] = a4;
+        }
+
+    for(b = 0; b < 4; b++){
+        a1=A[i+4+b][j+4], a2=A[i+4+b][j+5], a3=A[i+4+b][j+6], a4=A[i+4+b][j+7];
+        B[j+4][i+4+b] = a1;
+        B[j+5][i+4+b] = a2;
+        B[j+6][i+4+b] = a3;
+        B[j+7][i+4+b] = a4;
+    }
+
+    //处理第一列和第一行剩下7×2个8×8块
+    j=0;
+    for(i = 8; i < M; i += 8){
+        for(t = i; t < i + 4; t++)
+            for(b = j; b < j + 4; b++){
+                B[b][t] = A[t][b];
+            }
+        for(t = i ; t < i + 4; t++)
+            for(b = j + 4; b < j + 8; b++){
+                B[b][t] = A[t][b];
+            }
+        for(t = i + 4; t < i + 8; t++)
+            for(b = j + 4; b < j + 8; b++)
+                B[b][t] = A[t][b];
+        for(t = i + 4; t < i + 8; t++)
+            for(b = j; b < j + 4; b++)
+                B[b][t] = A[t][b];
+    }
+    i=0;
+    for(j = 8; j < M; j += 8){
+        for(t = i; t < i + 4; t++)
+            for(b = j; b < j + 4; b++){
+                B[b][t] = A[t][b];
+            }
+        for(t = i ; t < i + 4; t++)
+            for(b = j + 4; b < j + 8; b++){
+                B[b][t] = A[t][b];
+            }
+        for(t = i + 4; t < i + 8; t++)
+            for(b = j + 4; b < j + 8; b++)
+                B[b][t] = A[t][b];
+        for(t = i + 4; t < i + 8; t++)
+            for(b = j; b < j + 4; b++)
+                B[b][t] = A[t][b];
+    }
+}
+
+char transpose64_diagonal_desc[] = "Transpose64 diagonal+orther";
+void transpose64_diagonal(int M, int N, int A[N][M], int B[M][N]){
+    for(int i = 0; i < N; i += 8)
+        for(int j = 0; j < M; j += 8){
+            int a1, a2, a3, a4, a5, a6, a7, a8, t, b;
+            if(i == j){   
+            //B左上矩阵
+            for(t = 0; t < 4; t++){
+                a1=A[i+t][j], a2=A[i+t][j+1], a3=A[i+t][j+2], a4=A[i+t][j+3];
+                a5=A[i+t][j+4], a6=A[i+t][j+5], a7=A[i+t][j+6], a8=A[i+t][j+7];
+                B[j][i+t] = a1;
+                B[j+1][i+t] = a2;
+                B[j+2][i+t] = a3;
+                B[j+3][i+t] = a4;
+                //为防止冲突，剩下四个先放在右边4×4的子矩阵
+                B[j][i+t+4] = a5;
+                B[j+1][i+t+4] = a6;
+                B[j+2][i+t+4] = a7;
+                B[j+3][i+t+4] = a8;
+            }
+            //B左下矩阵，将右边的子矩阵放到下面
+            for(t = 0; t < 4; t++){
+                a1 = B[j+t][i+4];
+                a2 = B[j+t][i+5];
+                a3 = B[j+t][i+6];
+                a4 = B[j+t][i+7];
+                B[j+4+t][i] = a1;
+                B[j+4+t][i+1] = a2;
+                B[j+4+t][i+2] = a3;
+                B[j+4+t][i+3]= a4;
+            }
+            //处理B右上、右下两个子矩阵，完全冲突，只能4×4的块来搞。
+            //也就是处理A左下和右下两个子矩阵。
+            for(b = 0; b < 4; b++){
+                    a1=A[i+4+b][j], a2=A[i+4+b][j+1], a3=A[i+4+b][j+2], a4=A[i+4+b][j+3];
+                    B[j][i+4+b] = a1;
+                    B[j+1][i+4+b] = a2;
+                    B[j+2][i+4+b] = a3;
+                    B[j+3][i+4+b] = a4;
+                }
+
+             for(b = 0; b < 4; b++){
+                    a1=A[i+4+b][j+4], a2=A[i+4+b][j+5], a3=A[i+4+b][j+6], a4=A[i+4+b][j+7];
+                    B[j+4][i+4+b] = a1;
+                    B[j+5][i+4+b] = a2;
+                    B[j+6][i+4+b] = a3;
+                    B[j+7][i+4+b] = a4;
+                }
+            }
+            else{
+                //  其他块，A和B的缓存项不冲突。
+                //但是A和B自身 8×8的块，上下两个4×4的块冲突。
+                for(t = i; t < i + 4; t++)
+                    for(b = j; b < j + 4; b++){
+                        B[b][t] = A[t][b];
+                    }
+                for(t = i ; t < i + 4; t++)
+                    for(b = j + 4; b < j + 8; b++){
+                        B[b][t] = A[t][b];
+                    }
+                for(t = i + 4; t < i + 8; t++)
+                    for(b = j + 4; b < j + 8; b++)
+                        B[b][t] = A[t][b];
+                for(t = i + 4; t < i + 8; t++)
+                    for(b = j; b < j + 4; b++)
+                        B[b][t] = A[t][b];
+            }
+        }
+}
+
+char transpose64_4block_desc[] = "Transpose64 4block and subblock";
+void transpose64_4block(int M, int N, int A[N][M], int B[M][N]){
+    for(int i = 0; i < N; i += 4 )
+        for(int j = 0; j < M; j += 4){
+            for(int b = 0; b < 4; b++){
+                int a1=A[i+b][j], a2=A[i+b][j+1], a3=A[i+b][j+2], a4=A[i+b][j+3];
+                B[j][i+b] = a1;
+                B[j+1][i+b] = a2;
+                B[j+2][i+b] = a3;
+                B[j+3][i+b] = a4;
+            }
+        }
+}
+
+char transpose64_8block_desc[] = "Transpose64 8block and subblock";
+void transpose64_8block(int M, int N, int A[N][M], int B[M][N]){
+    for(int i = 0; i < N; i += 8)
+        for(int j = 0; j < M; j += 8){
+            int a1, a2, a3, a4, a5, a6, a7, a8, t, b;
+            //B左上矩阵
+            for(t = 0; t < 4; t++){
+                a1=A[i+t][j], a2=A[i+t][j+1], a3=A[i+t][j+2], a4=A[i+t][j+3];
+                a5=A[i+t][j+4], a6=A[i+t][j+5], a7=A[i+t][j+6], a8=A[i+t][j+7];
+                B[j][i+t] = a1;
+                B[j+1][i+t] = a2;
+                B[j+2][i+t] = a3;
+                B[j+3][i+t] = a4;
+                //为防止冲突，剩下四个先放在右边4×4的子矩阵
+                B[j][i+t+4] = a5;
+                B[j+1][i+t+4] = a6;
+                B[j+2][i+t+4] = a7;
+                B[j+3][i+t+4] = a8;
+            }
+            //B左下矩阵，将右边的子矩阵放到下面
+            for(t = 0; t < 4; t++){
+                a1 = B[j+t][i+4];
+                a2 = B[j+t][i+5];
+                a3 = B[j+t][i+6];
+                a4 = B[j+t][i+7];
+                B[j+4+t][i] = a1;
+                B[j+4+t][i+1] = a2;
+                B[j+4+t][i+2] = a3;
+                B[j+4+t][i+3]= a4;
+            }
+            //处理B右上、右下两个子矩阵，完全冲突，只能4×4的块来搞。
+            //也就是处理A左下和右下两个子矩阵。
+            for(b = 0; b < 4; b++){
+                    a1=A[i+4+b][j], a2=A[i+4+b][j+1], a3=A[i+4+b][j+2], a4=A[i+4+b][j+3];
+                    B[j][i+4+b] = a1;
+                    B[j+1][i+4+b] = a2;
+                    B[j+2][i+4+b] = a3;
+                    B[j+3][i+4+b] = a4;
+                }
+
+             for(b = 0; b < 4; b++){
+                    a1=A[i+4+b][j+4], a2=A[i+4+b][j+5], a3=A[i+4+b][j+6], a4=A[i+4+b][j+7];
+                    B[j+4][i+4+b] = a1;
+                    B[j+5][i+4+b] = a2;
+                    B[j+6][i+4+b] = a3;
+                    B[j+7][i+4+b] = a4;
+                }
+        }
+}
 /* 
  * You can define additional transpose functions below. We've defined
  * a simple one below to help you get started. 
@@ -61,6 +412,13 @@ void registerFunctions()
     /* Register any additional transpose functions */
     registerTransFunction(trans, trans_desc); 
 
+    //registerTransFunction(transpose64_8block, transpose64_8block_desc);
+
+    //registerTransFunction(transpose64_4block, transpose64_4block_desc);
+
+    //registerTransFunction(transpose64_diagonal, transpose64_diagonal_desc);
+
+    //registerTransFunction(transpose64_3part, transpose64_3part_desc);
 }
 
 /* 
